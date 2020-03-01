60b75fbf3933b369e829a53cba5c2dd774dd4445
New <jhonatan-n-santos@hotmail.com>
2020-03-01 13:42:57 -0300

    commit 10 + Buscar todos e buscar cliente (funcional)

--- app/src/main/java/com/example/carteiradeclientes/dominio/repositorio/ClienteRepositorio.java
+++ app/src/main/java/com/example/carteiradeclientes/dominio/repositorio/ClienteRepositorio.java
@@ -1,21 +1,23 @@
 package com.example.carteiradeclientes.dominio.repositorio;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.example.carteiradeclientes.dominio.entidades.Cliente;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteRepositorio {
 
     private SQLiteDatabase conexao;
 
-    public  ClienteRepositorio(SQLiteDatabase conexao){
+    public ClienteRepositorio(SQLiteDatabase conexao) {
         this.conexao = conexao;
     }
 
-    public void inserir(Cliente cliente){
+    public void inserir(Cliente cliente) {
 
         ContentValues contentValues = new ContentValues();
         contentValues.put("Nome", cliente.nome);
@@ -27,7 +29,7 @@ public class ClienteRepositorio {
 
     }
 
-    public void excluir(int codigo){
+    public void excluir(int codigo) {
 
         String[] parametros = new String[1];
         parametros[0] = String.valueOf(codigo);
@@ -36,7 +38,7 @@ public class ClienteRepositorio {
 
     }
 
-    public void alterar(Cliente cliente){
+    public void alterar(Cliente cliente) {
 
         ContentValues contentValues = new ContentValues();
         contentValues.put("Nome", cliente.nome);
@@ -51,13 +53,69 @@ public class ClienteRepositorio {
 
     }
 
-    public List<Cliente> buscarTodos(){
+    public List<Cliente> buscarTodos() {
 
-        return null;
+        List<Cliente> clientes = new ArrayList<Cliente>();
+
+        StringBuilder sql = new StringBuilder();
+        sql.append(" SELECT CODIGO, NOME, ENDERECO, EMAIL, TELEFONE ");
+        sql.append("    FROM CLIENTE");
+
+        Cursor resultado = conexao.rawQuery(sql.toString(), null);
+
+        if (resultado.getCount() > 0) {
+
+            resultado.moveToFirst();
+
+            do {
+
+                Cliente cli = new Cliente();
+
+                cli.codigo = resultado.getInt(resultado.getColumnIndexOrThrow("CODIGO"));
+                cli.nome = resultado.getInt(resultado.getColumnIndexOrThrow("NOME"));
+                cli.endereco = resultado.getInt(resultado.getColumnIndexOrThrow("ENDERECO"));
+                cli.email = resultado.getInt(resultado.getColumnIndexOrThrow("EMAIL"));
+                cli.phone = resultado.getInt(resultado.getColumnIndexOrThrow("TELEFONE"));
+
+                clientes.add(cli);
+
+            } while (resultado.moveToNext());
+
+        }
+
+
+        return clientes;
     }
 
-    public Cliente buscarCliente(int codigo){
+    public Cliente buscarCliente(int codigo) {
+
+        Cliente cliente = new Cliente();
+
+        StringBuilder sql = new StringBuilder();
+        sql.append(" SELECT CODIGO, NOME, ENDERECO, EMAIL, TELEFONE ");
+        sql.append("    FROM CLIENTE");
+        sql.append(" WHERE CODIGO = ? ");
+
+        String[] parametros = new String[1];
+        parametros[0] = String.valueOf(codigo);
+
+        Cursor resultado = conexao.rawQuery(sql.toString(), parametros);
+
+        if (resultado.getCount() > 0) {
+
+            resultado.moveToFirst();
+
+            cliente.codigo = resultado.getInt(resultado.getColumnIndexOrThrow("CODIGO"));
+            cliente.nome = resultado.getInt(resultado.getColumnIndexOrThrow("NOME"));
+            cliente.endereco = resultado.getInt(resultado.getColumnIndexOrThrow("ENDERECO"));
+            cliente.email = resultado.getInt(resultado.getColumnIndexOrThrow("EMAIL"));
+            cliente.phone = resultado.getInt(resultado.getColumnIndexOrThrow("TELEFONE"));
+
+            return cliente;
+        }
 
         return null;
+
     }
 }
+
